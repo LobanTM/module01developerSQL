2d62863823a7c249398ec56cabb346ec84748b95
LobanTM <loban2010@meta.ua>
2017-08-18 16:31:43 +0300

    Update 5.sql

--- 5.sql
+++ 5.sql
@@ -1,7 +1,10 @@
 /*Найти клиента (customer), которьій приносит меньше всего 
 прибыли компании (company) для каждой из компаний */
-select com.company, cu.customer, sum(p.cost)
-from module01.companies com
-inner join module01.projects p on p.company = com.id
-inner join module01.customers cu on p.customer = cu.id
-group by cu.customer
+select q.company, min(S), q.customer
+from(select com.company, p.project, sum(p.cost) as S, cu.customer
+	from module01.companies com
+	inner join module01.projects p on p.company = com.id
+	inner join module01.customers cu on p.customer = cu.id    
+	group by cu.customer
+    order by s) q
+group by q.company    
